926ed517050ff3bd29209424df70a77b7809bd9f
Nathan Soucier <soucienp@gmail.com>
2024-11-12 11:56:16 -0500

    making first playwright test

---
+++ my-first-playwright-script/login.js
@@ -0,0 +1,12 @@
+const { chromium } = require('playwright');
+
+(async () => {
+    const browser = await chromium.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto('https://danube-webshop.herokuapp.com/')
+
+    await page.waitForTimeout(5000)
+
+    await browser.close();
+
+})()
