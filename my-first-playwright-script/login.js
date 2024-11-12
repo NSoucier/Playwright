f819a45197cf5429c7b8ef55601783b6823466be
Nathan Soucier <soucienp@gmail.com>
2024-11-12 12:08:20 -0500

    added first few login tests

--- my-first-playwright-script/login.js
+++ my-first-playwright-script/login.js
@@ -1,12 +1,17 @@
 const { chromium } = require('playwright');
 
 (async () => {
-    const browser = await chromium.launch({ headless: false });
+    const browser = await chromium.launch();
     const page = await browser.newPage();
     await page.goto('https://danube-webshop.herokuapp.com/')
 
-    await page.waitForTimeout(5000)
+    await page.click('#login')
+    await page.type('#n-email', 'user@email.com')
+    await page.type('#n-password2', 'supersecure1')
 
+    await page.click('#goto-signin-btn')
+
+    await page.waitForSelector('#login-message', { state: 'visible' })
     await browser.close();
 
 })()
